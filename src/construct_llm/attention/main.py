8bbcab6058d2ae5ce793a0331dd640b2406b391a
Mingeax <wsyym@qq.com>
2026-09-13 20:15:46 +0800

    feat-wip: 因果注意力

--- src/construct_llm/attention/main.py
+++ src/construct_llm/attention/main.py
@@ -126,3 +126,48 @@ torch.manual_seed(123)
 sa_v1 = SelfAttention_v1(d_in, d_out)
 print("sa_v1(inputs): ", sa_v1(inputs))
 
+
+# 使用nn.Linear层进一步优化SelfAttention_v1的实现
+class SelfAttention_v2(nn.Module):
+    def init(self, d_in, d_out, qkv_bias=False):
+        super().__init__()
+        # nn.Linear提供了优化过的权重初始方案
+        self.W_query = nn.Linear(torch.rand(d_in, d_out), requires_grad=False)
+        self.W_key = nn.Linear(torch.rand(d_in, d_out), requires_grad=False)
+        self.W_value = nn.Linear(torch.rand(d_in, d_out), requires_grad=False)
+
+    def forward(self, x):
+        keys = self.W_key(x)
+        queries = self.W_query(x)
+        values = self.W_value(x)
+        attn_scores = queries @ keys.T
+        attn_weights = torch.softmax(attn_scores / keys.shape[-1] ** 0.5, dim=-1)
+        context_vec = attn_weights @ values
+        return context_vec
+
+
+# 使用方法同SelfAttention_v1
+torch.manual_seed(789)
+sa_v2 = SelfAttention_v2(d_in, d_out)
+print(sa_v2(inputs))
+
+# 因果注意力(掩码注意力)
+queries = sa_v2.W_query(inputs)
+keys = sa_v2.W_key(input)
+attn_scores = queries @ keys.T
+attn_weights = torch.softmax(attn_scores / keys.shape[-1] ** 0.5, dim=-1)
+
+print("attn_weights:", attn_weights)
+
+context_length = attn_scores.shape[0]
+# 获得下半角方阵, 元素为1和0
+mask_simple = torch.tril(torch.ones(context_length, context_length))
+print("mask_simple: ", mask_simple)
+
+masked_simple = attn_weights * mask_simple
+print("masked_simple: ", masked_simple)
+
+# 重新归一化注意力权重
+row_nums = masked_simple.sum(dim=-1, keepdim=True)
+masked_simple_norm = masked_simple / row_nums
+print("masked_simple_norm: ", masked_simple_norm)
